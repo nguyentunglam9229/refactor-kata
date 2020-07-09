2699916701f8d8f1f551474d9dbd7d34276fe53a
lam <nguyentunglam9229@gmail.com>
2020-07-09 10:52:51 +0700

    branch for each case:

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,5 +1,6 @@
-let Sulfuras =  require('./Sulfuras')
-let AgedBrie =  require('./AgedBrie')
+let Sulfuras = require('./Sulfuras')
+let AgedBrie = require('./AgedBrie')
+
 class Item {
     constructor(name, sellIn, quality) {
         this.name = name;
@@ -15,34 +16,62 @@ class Shop {
 
     updateQuality() {
         this.items.forEach(item => {
-            if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.quality > 0) {
-                    if (!this.isLengendaryItem(item)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            if (item.name === 'Sulfuras, Hand of Ragnaros') {
+
+            } else if (item.name === 'Aged Brie') {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-                    if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                    }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
                             }
                         }
                     }
                 }
-            }
-            if (!this.isLengendaryItem(item)) {
                 item.sellIn = item.sellIn - 1;
-            }
-            if (item.sellIn < 0) {
-                if (item.name !== 'Aged Brie') {
+
+                if (item.sellIn < 0) {
                     if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
                         if (item.quality > 0) {
                             if (!this.isLengendaryItem(item)) {
@@ -51,10 +80,6 @@ class Shop {
                         }
                     } else {
                         item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
                     }
                 }
             }
