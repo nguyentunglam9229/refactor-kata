27034bdac4f6525fba9dc7e0c60657ccaf2957c0
lam <nguyentunglam9229@gmail.com>
2020-07-09 10:53:57 +0700

    update branch"

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -48,38 +48,16 @@ class Shop {
                     item.quality = item.quality - item.quality;
                 }
             } else {
-                if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-                        }
-                    }
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
                 item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
-                    if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.quality > 0) {
-                            if (!this.isLengendaryItem(item)) {
-                                item.quality = item.quality - 1;
-                            }
+                    if (item.quality > 0) {
+                        if (!this.isLengendaryItem(item)) {
+                            item.quality = item.quality - 1;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
                 }
             }
